5093e7a53c0a523fbd7259c23e1f47fc4976d2c6
NguyenHongQuan003 <nhquan2622003@gmail.com>
2023-03-31 20:13:28 +0700

    update 8:31 31/3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-$(document).ready(function(){
-    $("#toggle").click(function(){
-        $("nav").slideToggle();
-    })
-})
+
 
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
 registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
+wrapper.classList.add('active');
 });
 
 loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
+wrapper.classList.remove('active');
 });
 
 // Kiểm tra form đăng ký
