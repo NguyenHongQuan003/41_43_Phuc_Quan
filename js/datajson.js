2686ff89a981432164139b502493295f3e421ed6
NguyenHongQuan003 <nhquan2622003@gmail.com>
2023-04-02 18:21:47 +0700

    update 6:21 2/4

--- js/datajson.js
+++ js/datajson.js
@@ -18,6 +18,16 @@ let sp2 = {
         '-Bộ đầy đủ: gồm phụ kiện bình thường + có thêm bảng chữ cái,số,con vật bằng nam châm , dòng kẻ ô ly ,tạp giề bao tay, và thêm 8 chiếc bút  màu....'
 }
 
-let dssp = [sp1, sp2]
+let sp3 = {
+    masp: 3,
+    tensp: "Camry 2.5HV Hybrid 2023",
+    hinhanh: '../img/toyota/p2-1.jpg',
+    dongia: "1.460.000.000 VND",
+    mota: 'Phụ kiện đi kèm với bảng bên em gồm:' +
+        '-Bộ bình thường gồm: 5 chiếc bút màu,1 hộp sáp màu ,2 miếng lau bảng, 1 kẹp phấn, và 8 miếng nam châm' +
+        '-Bộ đầy đủ: gồm phụ kiện bình thường + có thêm bảng chữ cái,số,con vật bằng nam châm , dòng kẻ ô ly ,tạp giề bao tay, và thêm 8 chiếc bút  màu....'
+}
+
+let dssp = [sp1, sp2, sp3]
 
 localStorage.setItem("dssp", JSON.stringify(dssp))
