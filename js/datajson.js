e4f21cdca4579bd1712d29f49b620d71332b6665
NguyenHongQuan003 <nhquan2622003@gmail.com>
2023-04-04 13:02:02 +0700

    update 1:1 4/4

--- js/datajson.js
+++ js/datajson.js
@@ -3,31 +3,261 @@ let sp1 = {
     tensp: "Vios1.5GR-S (thể thao)",
     hinhanh: '../img/toyota/p1-1.jpeg',
     dongia: "641.000.000 VND",
-    mota: 'Phụ kiện đi kèm với bảng bên em gồm:' +
-        '-Bộ bình thường gồm: 5 chiếc bút màu,1 hộp sáp màu ,2 miếng lau bảng, 1 kẹp phấn, và 8 miếng nam châm' +
-        '-Bộ đầy đủ: gồm phụ kiện bình thường + có thêm bảng chữ cái,số,con vật bằng nam châm , dòng kẻ ô ly ,tạp giề bao tay, và thêm 8 chiếc bút  màu....' +
-        '-Giúp các mẹ giáo dục và dạy con học tập một cách tốt nhất'
+    mota: 'Toyota Vios là mẫu xe hạng nhỏ (B) dành cho cá '+
+    'nhân và gia đình của hãng Toyota. Nổi tiếng với giá cả '+
+    'rẻ nhất, kích thước gọn gàng, giúp di chuyển linh hoạt '+
+    'ngay cả trong phố đông, khả năng tiêu hao nhiên liệu '+
+    'đáng mơ ước (chỉ 5,8 lít/100km). Toyota Vios 2023 được '+
+    'nâng cấp với ngoại thất bắt mắt hơn, nội thất và tiện '+
+    'nghi với chìa khoá khởi động thông minh, Cruise Control, '+
+    'đầu DVD, camera lùi, cân bằng điện tử, hỗ trợ khởi hành '+
+    'ngang dốc, kiểm soát lực kéo... An toàn với 7 túi khí.<br><br>'+
+    '- Xe có 6 màu: Đen, Nâu vàng, Bạc, Trắng, Đỏ, Xám.<br><br>'+
+    '- Ưu điểm khi so với đối thủ Honda City, Hyundai Accent, '+
+    'Mazda 2: Kiểu dáng đẹp, lành, thích hợp với nhiều đối '+
+    'tượng khách hàng. Xe bền bỉ, ít hỏng vặt, thay thế phụ '+
+    'tùng hay bao dưỡng rất rẻ, tiết kiệm nhiên liệu, "vô '+
+    'địch" về giữ giá<br><br>'+
+    '- Nhược điểm: chưa có cửa sổ trời.'
 }
 let sp2 = {
     masp: 2,
     tensp: "Camry 2.5HV Hybrid 2023",
     hinhanh: '../img/toyota/p2-1.jpg',
     dongia: "1.460.000.000 VND",
-    mota: 'Phụ kiện đi kèm với bảng bên em gồm:' +
-        '-Bộ bình thường gồm: 5 chiếc bút màu,1 hộp sáp màu ,2 miếng lau bảng, 1 kẹp phấn, và 8 miếng nam châm' +
-        '-Bộ đầy đủ: gồm phụ kiện bình thường + có thêm bảng chữ cái,số,con vật bằng nam châm , dòng kẻ ô ly ,tạp giề bao tay, và thêm 8 chiếc bút  màu....'
+    mota: 'Toyota Camry là mẫu sedan cỡ trung(D) tại thị '+
+    'trường Việt nam, cạnh tranh với đối thủ như Honad Accord, '+
+    'Kia Optima, Mazda 6. Toyota Camry 2023 ra mắt tại Việt '+
+    'Nam tháng 12-2021, được nhập khẩu từ Thái Lan với 4 '+
+    'phiên bản là: 2.5HV (hybrid); 2.5Q; 2.0Q và 2.0G. Thiết '+
+    'kế mới theo phong cách Lexus, đèn full LED. Nội thất sang '+
+    'trọng, tiện nghi với màn hình DVD 8inch, 9 loa JBL, cửa sổ '+
+    'trời, điều hòa 3 vùng tự động. Camry 2023 có khá nhiều công '+
+    'nghệ tiên tiến như cân bằng điện tử VSC, Cruise Control, '+
+    'cảnh báo điểm mù, phanh tay điện tử, cảnh báo lệch làn '+
+    'và hỗ trợ giữ làn đường, camera 360...Xe có các màu: Đen, '+
+    'Đỏ,Trắng Ngọc trai.<br><br>'+
+    ''
 }
 
 let sp3 = {
     masp: 3,
-    tensp: "Camry 2.5HV Hybrid 2023",
-    hinhanh: '../img/toyota/p2-1.jpg',
-    dongia: "1.460.000.000 VND",
-    mota: 'Phụ kiện đi kèm với bảng bên em gồm:' +
-        '-Bộ bình thường gồm: 5 chiếc bút màu,1 hộp sáp màu ,2 miếng lau bảng, 1 kẹp phấn, và 8 miếng nam châm' +
-        '-Bộ đầy đủ: gồm phụ kiện bình thường + có thêm bảng chữ cái,số,con vật bằng nam châm , dòng kẻ ô ly ,tạp giề bao tay, và thêm 8 chiếc bút  màu....'
+    tensp: "Avanza MT",
+    hinhanh: '../img/toyota/p3-1.jpg',
+    dongia: "558.000.000 VND",
+    mota: 'Toyota Avanza là mẫu xe đa dụng MPV cỡ nhỏ, đàn em '+
+    'của Toyota Innova. Avanza 2023 ra mắt trong tháng 03-2022 '+
+    'với 2 phiên bản là Avanza Premio MT và Avanza Premio CVT. '+
+    'Xe vẫn có 7 chỗ, kích thước tổng thể DxRxC là 4395 x 1730 x '+
+    '1700 mm, chiều dài cơ sở 2750mm; khoảng sáng gầm 205mm. '+
+    'Avanza hiện đã được lắp ráp tại Việt Nam.<br><br>'
 }
 
-let dssp = [sp1, sp2, sp3]
+let sp4 = {
+    masp: 4,
+    tensp: "Veloz CrossT",
+    hinhanh: '../img/toyota/p4-1.jpg',
+    dongia: "658.000.000 VND",
+    mota: 'Toyota Veloz là dòng xe MPV lai SUV 7 chỗ cỡ nhỏ, biến '+
+    'thể cao cấp của chiếc Toyota Avanza. Veloz 2023 là thế hệ thứ '+
+    '3 hoàn toàn mới, có thiết kế nội ngoại thất hiện đại hơn và có '+
+    'nhiều điểm khác biệt so với bản tiêu chuẩn. Toyota Veloz 2023 sử '+
+    'dụng động cơ xăng 4 xy lanh, 1.5 lít hút khí tự nhiên, sản sinh '+
+    'ra công suất 105 mã lực tại 6.000 vòng/ phút và mô men xoắn cực '+
+    'đại 138Nm tại 4.200 vòng/ phút. Động cơ đi kèm hộp số vô cấp CVT, '+
+    'dẫn động cầu trước tiêu chuẩn.'
+}
+
+let sp5 = {
+    masp: 5,
+    tensp: "Raize",
+    hinhanh: '../img/toyota/p5-1.jpg',
+    dongia: "552.000.000 VND",
+    mota: 'Toyota Raize là dòng xe SUV cỡ nhỏ (B), cạnh tranh với '+
+    'Hyundai Kona, Kia Sonet...Raize sử dụng động cơ 1.0L Turbo cho '+
+    'công suất 98 sức ngựa. Hộp số tự động CVT. Ngoại thất trẻ trung '+
+    'bắt mắt với đầu xe vuông vức; nội thất với đầu DVD 9inch kết hợp '+
+    'Apple Car/Androi Auto...Hệ thống an toàn TSS nổi bật với cân '+
+    'bằng điện tử, phanh tự động, kiểm soát hành trình chủ động....'
+}
+
+let sp6 = {
+    masp: 6,
+    tensp: "Corolla Cross 1.8G",
+    hinhanh: '../img/toyota/p6-1.jpeg',
+    dongia: "755.000.000 VND",
+    mota: 'Corolla Cross là dòng xe Crossover 5 chỗ hạng (C-) của '+
+    'Toyota, ra mắt tại Việt Nam ngày 05/08/2020. Corolla Cross cạnh '+
+    'tranh với các đối thủ như Mazda CX5, Hyundai Tucson và cả '+
+    'Hyundai Kona, Honda HRV. Corolla Cross máy xăng trang bị động '+
+    'cơ xăng 1.8L công suất 140Hp và mô-men xoắn 175 Nm. Bản Hybrid '+
+    'có thêm motor điện tạo công suất tổng 180Hp. Hệ dẫn động cầu trước '+
+    'và hộp số CVT. Kích thước xe 4.460 x 1.825 x 1.620 (mm), chiều dài '+
+    'trục cơ sở 2.640 mm và khoảng sáng gầm 161 mm.'
+}
+
+
+let sp7 = {
+    masp: 7,
+    tensp: "Honda City 1.5 RS",
+    hinhanh: '../img/honda/p1-2.jpg',
+    dongia: "599.000.000 VND",
+    mota: 'Xe Honda City 1.5 RS là một trong những phiên bản cao cấp của '+
+    'dòng xe sedan Honda City. Xe được trang bị động cơ xăng i-VTEC 1.5L, '+
+    'cho công suất tối đa là 118 mã lực và mô-men xoắn cực đại 145 Nm. '+
+    'Động cơ này được kết hợp với hộp số tự động vô cấp CVT.<br><br>'+
+    'Nội thất của xe được trang bị rất đầy đủ tiện nghi và sang trọng, '+
+    'với ghế da cao cấp, màn hình hiển thị đa thông tin kích thước 7 inch, '+
+    'hệ thống âm thanh 8 loa, điều hòa tự động, và cửa sổ trời điều khiển '+
+    'điện. Xe cũng có các tính năng an toàn như hệ thống phanh ABS, cân '+
+    'bằng điện tử ESC, hỗ trợ khởi hành ngang dốc HSA, hỗ trợ đổ đèo, '+
+    'cảm biến lùi, camera lùi, và 6 túi khí.'
+}
+
+let sp8 = {
+    masp: 8,
+    tensp: "Honda HR-V 1.8L",
+    hinhanh: '../img/honda/p2-2.jpg',
+    dongia: "871.000.000 VND",
+    mota: 'Xe Ô tô Honda HR-V là chiếc SUV đô thị lần đầu tiên được giới '+
+    'thiệu tại thị trường Nhật Bản vào năm 2014 và nhanh chóng trở thành 1 '+
+    'trong những sản phẩm thành công nhất của Honda. Cho đến nay, xe đã có '+
+    'doanh số cộng dồn lên tới hơn 2 triệu chiếc – một con số ấn tượng '+
+    'khiến đông đảo đối thủ phải ao ước.<br><br>'+
+    'Honda HR-V được trang bị khối động cơ 1.8L i-VTEC, cho công suất tối '+
+    'đa 141 mã lực tại 6500 vòng/phút và mô-men xoắn cực đại 172 Nm tại '+
+    '4300 vòng/phút. Sức mạnh của xe được kết hợp với hộp số vô cấp CVT '+
+    'ứng dụng công nghệ Earth Dreams Technology và hệ dẫn động cầu trước '+
+    'cho khả năng vận hành êm ái, mượt mà nhưng vẫn có sự phấn khích cần '+
+    'thiết. '
+}
+
+let sp9 = {
+    masp: 9,
+    tensp: "Honda Civic 1.5G",
+    hinhanh: '../img/honda/p3-2.jpg',
+    dongia: "770.000.000 VND",
+    mota: 'Xe Ô tô Honda Civic là mẫu Sedan cỡ C duy nhất được nhập khẩu '+
+    'nguyên chiếc và phân phối tại thị trường Việt Nam hiện nay.<br><br>'+
+    'Xe Honda Civic đời mới không có nhiều thay đổi so với thế hệ tiền '+
+    'nhiệm và vẫn mang trong mình hàng loạt ưu điểm vượt trội như: Thiết '+
+    'kế lịch lãm phù hợp với thị hiếu của đông đảo khách hàng, nhiều '+
+    'trang bị tiện nghi và trải nghiệm vận hành luôn được đánh giá cao.<br><br>'+
+    'Khách hàng sẽ có 2 tùy chọn bao gồm 1.8L SOHC 4 xy lanh và 1.5L '+
+    'DOHC 4 xy lanh. Hai động cơ này có thể tạo ra công suất tối đa '+
+    '139-170 mã lực, mô men xoắn cực đại 174-220 Nm. Cỗ máy trên được '+
+    'kết hợp với hộp số vô cấp CVT quen thuộc. '
+}
+
+let sp10 = {
+    masp: 10,
+    tensp: "Honda CRV 1.5 L",
+    hinhanh: '../img/honda/p4-2.jpg',
+    dongia: "1.118.000.000 VND",
+    mota: 'Luôn là sản phẩm được yêu thích bởi đông đảo khách hàng Việt '+
+    'Nam, Xe Ô tô Honda CR-V thường xuyên lọt top 10 cái tên có doanh '+
+    'số tốt nhất tháng và đôi khi còn giữ vị trí quán quân đầy tự hào. <br><br>'+
+    'Chiếc SUV ghi điểm nhờ ngôn ngữ thiết kế đẹp mắt phù hợp với thị '+
+    'hiếu của khách hàng, bên cạnh đó là sự ổn định và bền bỉ tuyệt đối '+
+    'trong quá trình vận hành. Ngoài ra, các trang bị của chiếc xe này '+
+    'cũng rất đầy đủ, hiện đại. <br><br>'+
+    'Xe Honda CRV trang bị khối động cơ tăng áp VTEC dung tích 1.5L kết '+
+    'hợp cùng hộp số CVT xây dựng từ nền tảng công nghệ Earth Dreams, '+
+    'công suất cực đại 188 mã lực tại 5.600 vòng/phút ấn tượng nhất '+
+    'phân khúc; mô-men xoắn cực đại đạt 240 Nm ở 2000 – 5000 vòng/phút. '+
+    'Với cỗ máy trên, xe có thể đạt vận tốc tối đa 200 km/h cùng khả '+
+    'năng đạt vận tốc 100 km/h trong thời gian 8,3 giây.'
+}
+
+let sp11 = {
+    masp: 11,
+    tensp: "Honda Accord",
+    hinhanh: '../img/honda/p5-2.jpg',
+    dongia: "1.319.000.000 VND",
+    mota: 'Xe Ô tô Honda Accord là chiếc Sedan nằm trong phân khúc D '+
+    'cạnh tranh với các đối thủ như Toyota Camry, Mazda 6 hay Kia Optima. <br><br>'+
+    'Mẫu xe này lần đầu tiên được Honda giới thiệu vào năm 1976, trải '+
+    'qua 9 thế hệ đã có gần 20 triệu chiếc Accord lăn bánh trên toàn thế '+
+    'giới. Con số này khiến đại diện tới từ Nhật Bản góp mặt trong top '+
+    '10 mẫu xe bán chạy nhất mọi thời đại đầy danh giá. Tại Việt Nam, '+
+    'xe được yêu thích bởi ngoại hình nam tính, lịch lãm đi cùng khả '+
+    'năng vận hành bền bỉ, ổn định. <br><br>'+
+    'Honda Accord mạnh mẽ được trang bị khối động cơ 2.4L DOHC i-VTEC, '+
+    '4 xi lanh thẳng hàng,16 van, ứng dụng công nghệ Earth Dreams '+
+    'Technology tân tiến. Cỗ máy này có công suất cực đại đạt 175 HP '+
+    'tại 6.200 v/p và mô men xoắn cực đại 225 Nm tại 4.000 v/p, kết '+
+    'hợp với hộp số tự động 5 cấp, giúp chiếc sedan vận hành ưu việt '+
+    'trên mọi cung đường với mức tiêu thụ nhiên liệu trung bình rơi '+
+    'vào khoảng 8 lít xăng/ 100 km đường kết hợp.'
+}
+
+let sp12 = {
+    masp: 12,
+    tensp: "New Mazda 6 Luxury",
+    hinhanh: '../img/mazda/p1-3.webp',
+    dongia: "829.000.000 VND",
+    mota: 'Xe ô tô New Mazda 6 Luxury là một chiếc sedan hạng trung cao '+
+    'cấp của hãng Mazda. Xe được thiết kế với phong cách Kodo - Soul '+
+    'of Motion, mang đến một diện mạo sang trọng và hiện đại. <br><br>'+
+    'Động cơ của Mazda 6 Luxury là động cơ xăng SkyActiv-G 2.5L, cho '+
+    'công suất cực đại 188 mã lực và mô-men xoắn cực đại 252 Nm. Động '+
+    'cơ này được kết hợp với hộp số tự động SkyActiv-Drive 6 cấp.<br><br>'+
+    'Nội thất của Mazda 6 Luxury được trang bị các tiện nghi cao cấp, '+
+    'bao gồm ghế da cao cấp, hệ thống âm thanh Bose Premium 11 loa, '+
+    'màn hình trung tâm kích thước 8 inch, hệ thống điều hòa tự động '+
+    '2 vùng, và hệ thống giải trí thông minh với Apple CarPlay và '+
+    'Android Auto. Xe còn có các tính năng an toàn như hệ thống '+
+    'phanh ABS, cân bằng điện tử ESC, hỗ trợ khởi hành ngang dốc '+
+    'HSA, cảm biến góc chết, cảm biến lùi, camera lùi, và 6 túi khí.'
+}
+
+let sp13 = {
+    masp: 13,
+    tensp: "New Mazda 6 2.0L Premium",
+    hinhanh: '../img/mazda/p2-3.webp',
+    dongia: "889.000.000 VND",
+    mota: 'Xe ô tô New Mazda 6 2.0L Premium là một chiếc sedan hạng '+
+    'trung của hãng xe Mazda, với thiết kế hiện đại và sang trọng.<br><br>'+
+    'Bên ngoài, xe được trang bị đèn pha LED tự động điều chỉnh góc chiếu, '+
+    'cản trước và sau thể thao, mâm đúc 19 inch, và đèn LED chạy ban '+
+    'ngày.<br><br>'+
+    'Bên trong, xe có nội thất bọc da cao cấp, ghế lái chỉnh điện 8 '+
+    'hướng, ghế phụ chỉnh điện 6 hướng, màn hình hiển thị đa thông tin '+
+    '8 inch, hệ thống âm thanh cao cấp Bose với 11 loa, hệ thống điều hòa '+
+    'tự động 2 vùng, cửa sổ trời, và hệ thống giải trí trên xe thông qua '+
+    'kết nối Bluetooth, USB và AUX.<br><br>'+
+    'Về động cơ, xe sử dụng động cơ xăng SkyActiv-G 2.0L, sản sinh công '+
+    'suất tối đa 155 mã lực và mô-men xoắn cực đại 200Nm. Xe sử dụng hộp '+
+    'số tự động 6 cấp và hệ thống lái trợ lực điện. Xe cũng được trang bị '+
+    'hệ thống an toàn i-ActivSense bao gồm cảnh báo động va chạm trước và '+
+    'sau, cảnh báo điểm mù, hỗ trợ đỗ xe tự động, hỗ trợ giữ làn đường, '+
+    'và hệ thống hỗ trợ phanh khẩn cấp.'
+}
+
+let sp14 = {
+    masp: 14,
+    tensp: "New Mazda 6 2.5L Signature Premium",
+    hinhanh: '../img/mazda/p3-3.webp',
+    dongia: "939.000.000 VND",
+    mota: 'Mazda 6 2.5L Signature Premium là một chiếc sedan hạng trung '+
+    'được sản xuất bởi hãng xe Mazda. Được giới thiệu như một phiên '+
+    'bản cao cấp của Mazda 6, chiếc xe này có nhiều tính năng và trang '+
+    'thiết bị tiêu chuẩn được cải tiến so với các phiên bản khác.<br><br>'+
+    'Xe được trang bị động cơ xăng SKYACTIV-G 2.5L, sản sinh công suất '+
+    'tối đa 186 mã lực và mô-men xoắn cực đại 252 Nm. Động cơ này được '+
+    'kết hợp với hộp số tự động 6 cấp, giúp cho chiếc xe vận hành mượt '+
+    'mà và tiết kiệm nhiên liệu.<br><br>'+
+    'Ngoại thất của Mazda 6 2.5L Signature Premium có thiết kế đẹp mắt '+
+    'và sang trọng với lưới tản nhiệt trapezoidal lớn và đèn pha LED. '+
+    'Bên trong, xe được trang bị ghế da cao cấp, hệ thống âm thanh Bose '+
+    '11 loa, màn hình cảm ứng 8 inch với đầu đọc CD/DVD, hệ thống giải '+
+    'trí và định vị điều hướng GPS.<br><br>'+
+    'Mazda 6 2.5L Signature Premium cũng được trang bị nhiều tính năng '+
+    'an toàn như hệ thống cảnh báo va chạm trước, hệ thống hỗ trợ phanh '+
+    'khẩn cấp, hệ thống cảnh báo điểm mù, cảnh báo lệch làn đường và hệ '+
+    'thống giám sát áp suất lốp.'
+}
+
+let dssp = [sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10, sp11, sp12, sp13, sp14]
 
 localStorage.setItem("dssp", JSON.stringify(dssp))
